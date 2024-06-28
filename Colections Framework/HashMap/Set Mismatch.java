55d8482c54e35e931d12e1e7b464c172bca15630
anunayreddy22 <156383908+anunayreddy22@users.noreply.github.com>
2024-06-28 13:42:59 -0500

    Update Set Mismatch.java

--- Colections Framework/HashMap/Set Mismatch.java
+++ Colections Framework/HashMap/Set Mismatch.java
@@ -1,29 +1,22 @@
 // Leetcode 645
-
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> mp = new HashMap<>();
-
-        for (int i = 1; i <= n; i++) {
-            mp.put(i, 0);
-        }
-
-        for (int a : nums) {
-            mp.put(a, mp.get(a) - 1);
-        }
-
-        int duplicate = 0, missing = 0;
-
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-            if (entry.getValue() == -1) {
-                duplicate = entry.getKey();
+        int ans[] = new int[2] ;
+        int n = nums.length ;
+        HashSet<Integer> set = new HashSet<>() ;
+        int sum = n*(n+1)/2 ;
+        int repeat = -1 ;
+        for(int val : nums){
+            if(set.contains(val)){
+                repeat = val ;
             }
-            if (entry.getValue() == 1) {
-                missing = entry.getKey();
+            else{
+                set.add(val) ;
+                sum -= val ;
             }
         }
-
-        return new int[]{duplicate, missing};
+        ans[0] = repeat ;
+        ans[1] = sum ;
+        return ans ;
     }
 }
